7ec164503ea6a26b94d9857b6dd9557ac9cb3bd2
adrianafowler <adrianafowler80@gmail.com>
2021-02-02 00:35:18 +0000

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log('Hi, thank you for checking my portfolio!'')
+console.log('Hi, thank you for checking my portfolio!')
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');

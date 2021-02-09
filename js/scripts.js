015f21634ad3eb8c7c55601f2f5bb6088754de02
adrianafowler <adrianafowler80@gmail.com>
2021-02-09 00:41:26 +0000

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,52 @@ function menuToggle() {
 
 
 
+$(document).ready(function(){
+  $('.responsive').slick({
 
-    
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+
+
+
+$(document).ready(function(){
+  $('.multiple-items').slick({
+  infinite: false,
+  slidesToShow: 1.5,
+  slidesToScroll: 1
+});
+	
+});

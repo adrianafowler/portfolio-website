01cd8d19ebabb511af7a4bc8e2bc70ad6a1e4e48
adrianafowler <adrianafowler80@gmail.com>
2021-02-16 23:37:34 +0000

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -37,7 +37,7 @@ $(document).ready(function(){
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
